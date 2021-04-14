af20dfeab55025835d766fcadc6bafcac34b65be
Josh Kinney <joshkinney@kinneykare.com>
2021-04-14 09:42:24 -0500

    Initial Commit

---
+++ LearningGit/LearningGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitApp.swift
+//  LearningGit
+//
+//  Created by Kinney Kare on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
